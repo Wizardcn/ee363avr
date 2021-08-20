138ac1aa9de7d348d5603af11fe240ff26b96f02
TONY <supawat125947@gmail.com>
2021-08-20 12:38:17 +0700

    ldi, movw, mov instructions

--- EE363avr/main.asm
+++ EE363avr/main.asm
@@ -1,12 +1,12 @@
-;
-; LabA_00_Demonstration
+; Data Transfer
+; instruction ldi, movw, mov
 ;
 
 	.org 0x000
 	nop
-	ldi r16, 0x12
+	ldi r16, 0x12		; Operand: Rd, Rr Operation: Rd <- Rr
 	ldi r17, 12
 	ldi r18, 0x0C
-	movw r1:r0, r17:r16
-	mov r2, r18
+	movw r1:r0, r17:r16 ; Operand: Rd, Rr Operation: Rd+1:Rd <- Rr+1:Rr
+	mov r2, r18			; Operand: Rd, K Operation: Rd <- K
 END: jmp END
