d2fa62d7112811bee292d93e667ce9e36f89dda8
TONY <supawat125947@gmail.com>
2021-08-24 17:12:19 +0700

    in, out instruction

--- EE363avr/main.asm
+++ EE363avr/main.asm
@@ -1,17 +1,17 @@
 ;
-; instruction lds, sts
-; topic 74, 121
-
-	.org 0x0000
+; instructiion in, out
+; topic 64, 88
+	.org	0x0000
 	nop
-	ldi		r16, 0xAA
-	ldi		r17, 0xBB
-	;--------------------
-	sts		0x0110, r16 ; r16 -> (x0110)
-	sts		0x0111, r17 ; r16 -> (x0111)
-	lds		r12, 0x0110 ; r12 <- (x0110)
-	lds		r13, 0x0111 ; r13 <- (x0111)
-	;--------------------
+	ldi		r16, 0xF0
+	ldi		r17, 0x55
+	ldi		r18, 0xFF
+	;----------------------------------------
+	out		0x04, r16	; r16 -> (x0024) IO(x04)
+	out		ox05, r17	; r17 -> (x0025) IO(x05)
 	nop
+	in		r18, 0x03	; r18 <- (x0023) IO(x03)
+	nop
+	;----------------------------------------
 END: jmp	END
 
