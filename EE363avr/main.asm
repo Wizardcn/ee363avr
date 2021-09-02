f3cd087e46d09a08cffe1a40a517ad458c7ae327
TONY <supawat125947@gmail.com>
2021-09-02 14:58:48 +0700

    ;

--- EE363avr/main.asm
+++ EE363avr/main.asm
@@ -1,16 +1,12 @@
 ;
-; instruction dec, inc, ser, clr, com, neg,  tst
-; topic 43, 48, 53, 65, 84, 110, 126 
+; instruction adiw, sbiw
+; topic 7, 102
 		.org	0x0000
 		nop
-		ldi		r16, 0x23
+		ldi		r26, 0xF8
+		ldi		r27, 0x01
 		;--------------------------------------------
-		dec		r16		;r16 <- r16-1
-		inc		r16		;r16 <- r16+1
-		ser		r17		;r17 <- 0xFF
-		clr		r17		;r17 <- 0x00
-		com		r16		;r16 <- 1's comp. of r16
-		neg		r16		;r16 <- 2's comp. of r16
-		tst		r16		;test r16 is zero
+		adiw	r27:r26, 20		;r27:r26 <- r27:r26 + 20
+		sbiw	r27:r26, 10		;r27:r26 <- r27:r26 - 10
 		;--------------------------------------------
 END:	jmp		END
