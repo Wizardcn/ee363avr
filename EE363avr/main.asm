3727cb08063bf70737410f3f3866b322806af6c0
TONY <supawat125947@gmail.com>
2021-09-02 15:21:22 +0700

    sbic, sbis, sbrc, sbrs instructions

--- EE363avr/main.asm
+++ EE363avr/main.asm
@@ -1,19 +1,24 @@
 ;
-; instruction brbc, brbs
-; topic 13, 14
+; instruction sbic, sbis, sbrc, sbrs
+; topic 100, 101, 104, 105
 		.org	0x0000
 		nop
-		ldi		r16, 200
-		ldi		r17, 50
-		ldi		r18, 3
+		ldi		r17, 6
+		ldi		r16, 0x01
+		out		0x04, r16
 		;--------------------------------------------
-		add		r16, r17
-		brbc	0, NEXT1
-		inc		r0
-NEXT1:	nop
-		add		r16, r18
-		brbs	0, NEXT2
-		inc		r0
-NEXT2:	nop
+		sbic	0x04, 0
+		inc		r17
+		nop
+		sbis	0x04, 0
+		dec		r17
+		nop
+		;--------------------------------------------
+		sbrc	r16, 4
+		inc		r17
+		nop
+		sbrs	r16, 4
+		dec		r17
+		nop
 		;--------------------------------------------
 END:	jmp		END
