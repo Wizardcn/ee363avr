660185cb869d0e94b1611c92049c50913006498a
TONY <supawat125947@gmail.com>
2021-09-02 11:27:42 +0700

    push, pop instructions

--- EE363avr/main.asm
+++ EE363avr/main.asm
@@ -1,17 +1,18 @@
 ;
-; instructiion in, out
-; topic 64, 88
-	.org	0x0000
+; instruction push, pop
+; topic 89, 90
+	.org 0x0000
 	nop
-	ldi		r16, 0xF0
-	ldi		r17, 0x55
-	ldi		r18, 0xFF
-	;----------------------------------------
-	out		0x04, r16	; r16 -> (x0024) IO(x04)
-	out		0x05, r17	; r17 -> (x0025) IO(x05)
-	nop
-	in		r18, 0x03	; r18 <- (x0023) IO(x03)
-	nop
-	;----------------------------------------
-END: jmp	END
+	ldi		r16, 0xAA
+	ldi		r17, 0xBB
+	ldi		r18, 0xCC
+	;------------------------------------------------------------
+	push	r16			; r16 -> (x08FF) SP:x08FE
+	push	r17			; r17 -> (x08FE) SP:x08FD
+	push	r18			; r18 -> (x08FD) SP:x08FC
+	pop				r4				; r4 <- (x08FD) SP:x08FD
+	pop				r5				; r5 <- (x08FE) SP:x08FE
+	pop				r6				; r6 <- (x08FF) SP:x08FF
+	;------------------------------------------------------------
+END: jmp			END
 
