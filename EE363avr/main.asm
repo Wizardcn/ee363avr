c1180baf251950a6827277c08946ef00fc8a27b4
TONY <supawat125947@gmail.com>
2021-09-02 15:07:45 +0700

    sec, seh, sei, sen, ses, set, sev, sez,

--- EE363avr/main.asm
+++ EE363avr/main.asm
@@ -1,10 +1,26 @@
 ;
-; instruction bclr, bset
-; topic 11, 34
+; instruction sec, seh, sei, sen, ses, set, sev, sez, 
+;             clc, clh, cli, cln, cls, clt, clv, clz
+; topic  39,  40,  41,  42,  44,  45,  46,  47, 
+;       106, 107, 108, 109, 111, 112, 113, 114
 		.org	0x0000
 		nop
 		;--------------------------------------------
-		bset	6
-		bclr	6
+		sec
+		seh
+		sei
+		sen
+		ses
+		set
+		sev
+		sez
+		clc
+		clh
+		cli
+		cln
+		cls
+		clt
+		clv
+		clz
 		;--------------------------------------------
 END:	jmp		END
