eec657d3a66af520b64293b73c316f1dd4a4a258
TONY <supawat125947@gmail.com>
2021-09-02 15:03:01 +0700

    cbi, sbi instructions

--- EE363avr/main.asm
+++ EE363avr/main.asm
@@ -1,18 +1,14 @@
 ;
-; instruction asr, lsl, lsr, rol, ror, swap
-; topic 10, 77, 78, 95, 96, 125
+; instruction cbi, sbi
+; topic 37, 99
 		.org	0x0000
 		nop
-		ldi		r16, 0x88
-		ldi		r17, 0x83
-		ldi		r18, 0x81
-		ldi		r19, 0x48
+		ldi		r16, 0x01
+		out		0x04, r16
 		;--------------------------------------------
-		asr		r16		;arithmetic shift right
-		lsl		r17		;logical shift left
-		lsr		r17		;logical shift right
-		rol		r18		;rotate left
-		ror		r18		;rotate right
-		swap	r19		;swap
+		sbi		0x05, 0		;set bit
+		cbi		0x05, 0		;clear bit
+		sbi		0x05, 0		;set bit
+		cbi		0x05, 0		;clear bit
 		;--------------------------------------------
 END:	jmp		END
