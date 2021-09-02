a20d32c2753bda2726113234fbae0bfe20dffebd
TONY <supawat125947@gmail.com>
2021-09-02 15:16:06 +0700

    brcc, brcs, breq, brge, brhc, brhs, brid, brie, brlo,

--- EE363avr/main.asm
+++ EE363avr/main.asm
@@ -1,18 +1,20 @@
 ;
-; instruction jmp, rjmp
-; topic  66, 94
+; instruction brcc, brcs, breq, brge, brhc, brhs, brid, brie, brlo, 
+;             brlt, brmi, brne, brpl, brsh, brtc, brts, brvc, brvs
+; topic 15, 16, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
 		.org	0x0000
 		nop
-		ldi		r16, 8
+		ldi		r16, 200
+		ldi		r17, 50
+		ldi		r18, 3
 		;--------------------------------------------
-		rjmp	LABEL1		
-		ldi		r16, 20
-LABEL1: dec		r16
-		jmp		LABEL2
-		;--------------------------------------------
-		.org	0x0010
-LABEL2: inc		r16		
-		nop
-		rjmp	LABEL2
+		add		r16, r17
+		brcc	NEXT1
+		inc		r0
+NEXT1:	nop
+		add		r16, r18
+		brcs	NEXT2
+		inc		r0
+NEXT2:	nop
 		;--------------------------------------------
 END:	jmp		END
