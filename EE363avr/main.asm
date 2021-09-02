05c4429bdab1e2271bcb4e22d43a08fdb61c386f
TONY <supawat125947@gmail.com>
2021-09-02 14:57:11 +0700

    dec, inc, ser, clr, com, neg,  tst intstructions

--- EE363avr/main.asm
+++ EE363avr/main.asm
@@ -1,15 +1,16 @@
 ;
-; instruction andi, cbr, ori, sbci, sbr, subi
-; topic 9, 38, 87, 98, 103, 124
+; instruction dec, inc, ser, clr, com, neg,  tst
+; topic 43, 48, 53, 65, 84, 110, 126 
 		.org	0x0000
 		nop
 		ldi		r16, 0x23
 		;--------------------------------------------
-		andi	r16, 0x0F	;r16 <- r16 # 0x0F
-		cbr		r16, 0x01	;r16 <- r16 # 0x01
-		ori		r16, 0x21	;r16 <- r16 # 0x21
-		sbci	r16, 0x10	;r16 <- r16 # 0x10
-		sbr		r16, 0x21	;r16 <- r16 # 0x21
-		subi	r16, 0x10	;r16 <- r16 # 0x10
+		dec		r16		;r16 <- r16-1
+		inc		r16		;r16 <- r16+1
+		ser		r17		;r17 <- 0xFF
+		clr		r17		;r17 <- 0x00
+		com		r16		;r16 <- 1's comp. of r16
+		neg		r16		;r16 <- 2's comp. of r16
+		tst		r16		;test r16 is zero
 		;--------------------------------------------
 END:	jmp		END
