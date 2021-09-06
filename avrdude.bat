9043f9daa3d9a2f37e7b6d888ddcc05e406acfb9
TONY <supawat125947@gmail.com>
2021-09-07 02:08:01 +0700

    change command and fix path of config file

--- avrdude.bat
+++ avrdude.bat
@@ -1,2 +1,2 @@
-avrdude -C "..\..\Program Files (x86)\Arduino\hardware\tools\avr\etc\avrdude.conf" -c arduino -p m328p -v -P COM7 -b 115200 -D -U flash:w:"C:\Users\ASUS\OneDrive - Chiang Mai University\Documents\Atmel Studio\7.0\EE363\Code\EE363avr\EE363avr\Debug\EE363avr.hex":i
-
+"C:\Program Files (x86)\Arduino\hardware\tools\avr/bin/avrdude" -C "C:\Program Files (x86)\Arduino\hardware\tools\avr\etc\avrdude.conf" -c arduino -p m328p -v -P COM7 -b 115200 -D -U flash:w:"C:\Users\ASUS\OneDrive - Chiang Mai University\Documents\Atmel Studio\7.0\EE363\Code\EE363avr\EE363avr\Debug\EE363avr.hex":i
+pause
