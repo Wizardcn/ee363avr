4caf2f50cca19870146b99d8c8ca32e943370a75
TONY <supawat125947@gmail.com>
2021-09-07 01:51:03 +0700

    code for test and script for burn code

--- EE363avr/main.asm
+++ EE363avr/main.asm
@@ -1,22 +1,13 @@
+; Test Tool: avrdude.exe
 		.org	0x0000
-		nop		
-		ldi		r16, 0x12
-		ldi		r17, 12
-		ldi		r18, 0x0C
-		ldi		r28, 0x02
-		ldi		r29, 0x01
-		.org	0x0008
-		movw	r1:r0, r17:r16
-		st		Y, r16
-		ld		r0, Y
-		std		Y+16, r3
-		ldd		r5, Y+1
-		out		0x04, r16	
-		dec		r16
-		inc		r16
-		sbi		0x05, 0
-		jmp		LABEL1
-		ldi		r16, 20
-LABEL1: dec		r16
-		nop
-XEND:	jmp		XEND
+		jmp		START
+;----------------------------------------
+START:
+		SBI		0x04, 5
+		CBI		0x07, 1
+LOOP:	SBIC	0x06, 1
+		SBI		0x05, 5
+		SBIS	0x06, 1
+		CBI		0x05, 5
+		RJMP	LOOP
+;----------------------------------------
---
+++ avrdude.bat
@@ -0,0 +1,5 @@
+@echo off
+
+avrdude -C "..\..\Program Files (x86)\Arduino\hardware\tools\avr\etc\avrdude.conf" -c arduino -p m328p -v -P COM7 -b 115200 -D -U flash:w:"C:\Users\ASUS\OneDrive - Chiang Mai University\Documents\Atmel Studio\7.0\EE363\Code\EE363avr\EE363avr\Debug\EE363avr.hex":i\
+
+pause
