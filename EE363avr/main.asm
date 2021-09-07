0ea574119cea43cca2b1f91d8c49484ed416ce2a
TONY <supawat125947@gmail.com>
2021-09-08 04:30:02 +0700

    code for PA01 in Programming part

--- EE363avr/main.asm
+++ EE363avr/main.asm
@@ -1,18 +1,25 @@
-		.org	0x0000
-		nop			
-		ldi		r16, 0x9B
-		ldi		r17, 0x16
-		ldi		r18, 0xC7
-		ldi		r19, 0x42
-		add		r16, r18
-		adc		r17, r19
-		eor		r18, r19
-		sub		r16, r18
-		sbc		r17, r19
-		and		r18, r17
-		or		r16, r17
-		andi	r16, 0x0F
-		cbr		r17, 0x01
-		ori		r16, 0xAA
-		sbci	r16, 0x19	
-END:	jmp		END	
+;
+;	Basic assembly program
+;
+			.org	0x0000
+			jmp				START
+		;------------------------------------------------
+START:	ldi			r16, 0x24
+			call	BCD2BIN
+		;------------------------------------------------
+END:	jmp			END
+
+;====================================================
+;input		r16
+;output		r0
+;resource	r17, r18
+BCD2BIN:
+			mov				r17, 16
+			andi	r16, 0x0F
+			swap	r17
+			andi	r17, 0x0F
+			ldi				r18, 10
+			mul				r17, r18
+			add				r0, r16
+			ret
+;====================================================
