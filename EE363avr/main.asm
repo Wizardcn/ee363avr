9bbc30b41cf1ea80c0a67cbed0e1c08f04a2284b
TONY <supawat125947@gmail.com>
2021-09-07 15:36:59 +0700

    Update for Q4B debugging

--- EE363avr/main.asm
+++ EE363avr/main.asm
@@ -1,13 +1,18 @@
-; Test Tool: avrdude.exe
 		.org	0x0000
-		jmp		START
-;----------------------------------------
-START:
-		SBI		0x04, 5
-		CBI		0x07, 1
-LOOP:	SBIC	0x06, 1
-		SBI		0x05, 5
-		SBIS	0x06, 1
-		CBI		0x05, 5
-		RJMP	LOOP
-;----------------------------------------
+		nop			
+		ldi		r16, 0x9B
+		ldi		r17, 0x16
+		ldi		r18, 0xC7
+		ldi		r19, 0x42
+		add		r16, r18
+		adc		r17, r19
+		eor		r18, r19
+		sub		r16, r18
+		sbc		r17, r19
+		and		r18, r17
+		or		r16, r17
+		andi	r16, 0x0F
+		cbr		r17, 0x01
+		ori		r16, 0xAA
+		sbci	r16, 0x19	
+END:	jmp		END	
